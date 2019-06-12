e004614729129227965b2db59fa76cd6b1c06195
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-06-12 11:12:33 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import TestComponent from './components/TestComponent';
-// import LoginPage from './components/LoginPage';
-// import ContentPage from './components/ContentPage';
+import LoginPage from './components/LoginPage';
+import ContentPage from './components/ContentPage';
 import './script';
 import { withMagicProps } from './HOCs/HOCs';
+import withAuthentication from './HOCs/Authentication';
 
 // Another way of using a HOC
 // We then invoke `EnhancedComponent` in the JSX below 
@@ -19,23 +20,23 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <>
-        <TestComponent originalProp="Test Prop" data={this.state.datas} />
-        <TestComponent originalProp="Test Prop" data={this.state.datas} />
-        <TestComponent originalProp="Test Prop" data={this.state.datas} />
-        <TestComponent originalProp="Test Prop" data={this.state.datas} />
-        <TestComponent originalProp="Test Prop" data={this.state.datas} />
-      </>
-    );
     // return (
-    //   <div className="App">
-    //     <h1>App is Authenticated: {  }</h1>
-    //       <LoginPage />
-    //       <ContentPage />
-    //   </div>
+    //   <>
+    //     <TestComponent originalProp="Test Prop" data={this.state.datas} />
+    //     <TestComponent originalProp="Test Prop" data={this.state.datas} />
+    //     <TestComponent originalProp="Test Prop" data={this.state.datas} />
+    //     <TestComponent originalProp="Test Prop" data={this.state.datas} />
+    //     <TestComponent originalProp="Test Prop" data={this.state.datas} />
+    //   </>
     // );
+    return (
+      <div className="App">
+        <h1>App is Authenticated: { this.props.isAuthed }</h1>
+          <LoginPage />
+          <ContentPage />
+      </div>
+    );
   }
 }
 
-export default App;
+export default withAuthentication(App);
--- src/HOCs/Authentication.jsx
+++ src/HOCs/Authentication.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
-export default function Authentication(Component) {
-  
+export default function withAuthentication(Component) {
+    return class Authentication extends React.Component {
+        constructor() {
+            super();
+            this.state = {
+                isAuthed: false
+            }
+        }
+
+        componentDidMount() {
+            if (localStorage.getItem('is_authed')) {
+                this.setState({ isAuthed: true });
+            }
+        }
+
+        render() {
+            return(
+                <Component 
+                    isAuthed={this.state.isAuthed}
+                />
+            );
+        }
+    }
 }
